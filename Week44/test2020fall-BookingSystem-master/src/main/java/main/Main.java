736774d2127ef112347700f49dfcab1a79ca7e10
Lukas-bjornvad <lukas.bjornvad@gmail.com>
2021-11-02 14:34:35 +0100

    Basic shit established for assignment 3

---
+++ Week44/test2020fall-BookingSystem-master/src/main/java/main/Main.java
@@ -0,0 +1,28 @@
+package main;
+
+import dto.Customer;
+import datalayer.customer.CustomerStorageImpl;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+
+    private static final String conStr = "jdbc:mysql://localhost:3307/mysql";
+    private static final String user = "root";
+    private static final String pass = "testuser123";
+
+    public static void main(String[] args) throws SQLException {
+        CustomerStorageImpl storage = new CustomerStorageImpl(conStr, user, pass);
+        //DriverManager.getConnection("jdbc:mysql://localhost:3307/?user=root&password=testuser123");
+        System.out.println("Got customers: ");
+        for(Customer c : storage.getCustomers()) {
+            System.out.println(toString(c));
+        }
+        System.out.println("The end.");
+    }
+
+    public static String toString(Customer c) {
+        return "{" + c.getId() + ", " + c.getFirstname() + ", " + c.getLastname() + "}";
+    }
+}
